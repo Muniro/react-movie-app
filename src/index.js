4022b7fae4c225184f9f2fdb35f6b7b4b29446ab
Muniro <Munir_Syed@hotmail.com>
2020-09-01 22:42:05 +0100

    add setup files and a reminder of redux boiler plate codes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//STORE --> GLOBALISED STATE
+
+
+
+//ACTION INCREMENT
+
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+
+
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT": return state + 10;
+    case "DECREMENT": return state - 10;
+    default: return 0;
+  }
+
+}
+
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+
+//DISPATCH
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
